963ef29bd24016236510f7c8956264acd77455cf
M1_42 <jeandellapasta@gmail.com>
2021-02-06 19:11:55 +0100

    Adding Dark Mode

--- src/firmware/firmwares.js
+++ src/firmware/firmwares.js
@@ -17,5 +17,21 @@ export const releases = {
         "0100": ["en", "fr", "nl", "pt", "it", "de", "es", "hu"],
       },
     },
+    {
+      name: "nsi.xyz-OS.1.1",
+      commit: "46658f7077045b28906e257f544414e41f62381a",
+      changelog: ["New: Just nsi.xyz-OS", "Fix: No fix", "Change: Dark-Mode"],
+      compatibility: {
+        N0110: true,
+        N0100: false,
+        web: false,
+        android: false,
+        "3ds": false,
+      },
+      available: true,
+      langages: {
+        "0100": ["en", "fr", "nl", "pt", "it", "de", "es", "hu"],
+      },
+    },
   ],
 };
