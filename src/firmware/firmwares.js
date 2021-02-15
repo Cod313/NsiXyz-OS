eb34a5e74f0c108cc7b29184e8a4ec9df447df8b
M1_42 <gabin.palix@icloud.com>
2021-02-15 17:01:01 +0100

    fgh

--- src/firmware/firmwares.js
+++ src/firmware/firmwares.js
@@ -33,5 +33,20 @@ export const releases = {
         "0100": ["en", "fr", "nl", "pt", "it", "de", "es", "hu"],
       },
     },
+    {
+      name: "nsi.xyz-OS.1.2",
+      commit: "46658f7077045b28906e257f544414e41f62381a",
+      changelog: ["New: Just nsi.xyz-OS", "Fix: No fix", "Change: Light-Mode++"],
+      compatibility: {
+        N0110: true,
+        N0100: false,
+        web: false,
+        android: false,
+        "3ds": false,
+      },
+      available: true,
+      langages: {
+        "0100": ["en", "fr", "nl", "pt", "it", "de", "es", "hu"],
+      },
   ],
 };
