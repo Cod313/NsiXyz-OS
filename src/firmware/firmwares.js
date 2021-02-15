63b85467b57e479928b9705d17ccdaf99bdf3852
M1_42 <gabin.palix@icloud.com>
2021-02-15 17:04:14 +0100

    chan

--- src/firmware/firmwares.js
+++ src/firmware/firmwares.js
@@ -2,7 +2,7 @@ export const releases = {
   latest: "nsi.xyz-OS.1.0",
   firmwares: [
     {
-      name: "nsi.xyz-OS.1.0",
+      name: "nsi.xyz-OS.1.2",
       commit: "46658f7077045b28906e257f544414e41f62381a",
       changelog: ["New: Just nsi.xyz-OS", "Fix: No fix", "Change: No change"],
       compatibility: {
@@ -33,20 +33,5 @@ export const releases = {
         "0100": ["en", "fr", "nl", "pt", "it", "de", "es", "hu"],
       },
     },
-    {
-      name: "nsi.xyz-OS.1.2",
-      commit: "46658f7077045b28906e257f544414e41f62381a",
-      changelog: ["New: Just nsi.xyz-OS", "Fix: No fix", "Change: Light-Mode++"],
-      compatibility: {
-        N0110: true,
-        N0100: false,
-        web: false,
-        android: false,
-        "3ds": false,
-      },
-      available: true,
-      langages: {
-        "0100": ["en", "fr", "nl", "pt", "it", "de", "es", "hu"],
-      },
   ],
 };
