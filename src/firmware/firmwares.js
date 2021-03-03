02f20d81b05d98cb91ada5503f63cc1361756130
Cod313 <jeandellapasta@gmail.com>
2021-03-03 19:04:05 +0100

    Update firmwares.js

--- src/firmware/firmwares.js
+++ src/firmware/firmwares.js
@@ -18,6 +18,22 @@ export const releases = {
       },
     },
     {
+      name: "nsi.xyz-OS.1.1_Bataille_navale",
+      commit: "46658f7077045b28906e257f544414e41f62381a",
+      changelog: ["New: ", "Ver: 2021-02-14", "Change: Game : Bataille navale"],
+      compatibility: {
+        N0110: true,
+        N0100: false,
+        web: false,
+        android: false,
+        "3ds": false,
+      },
+      available: true,
+      langages: {
+        "0100": ["en", "fr", "nl", "pt", "it", "de", "es", "hu"],
+      },
+    },
+    {
       name: "nsi.xyz-OS.1.1",
       commit: "46658f7077045b28906e257f544414e41f62381a",
       changelog: ["New: ", "Fix: No fix", "Change: Dark-Mode"],
